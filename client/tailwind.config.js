b78993cc33385361c68f717646eefa5d3f64eb97
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-07-18 18:28:45 +0100

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,15 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    // ...
-    flowbite.content(),
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [
-    // ...
-    flowbite.plugin(),
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
   ],
 };
