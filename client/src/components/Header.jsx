8a18cf50d87261dcd57e51d0b332be9527e8ec3a
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-07-19 20:52:20 +0100

    header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
     <Navbar className='border-b-2'>
       <Link
         to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:bg-gray-500'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-green-500 via-teal-500 to-cyan-500 rounded-lg text-white'>
           Abu Hanifa's
