3314e8914add504a974d45e2d17f9c29b08ab923
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-07-21 19:06:23 +0100

    UI functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import PrivateRoute from './components/PrivateRoute'
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-in' element={<SignUp />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />} >
         <Route path='/dashboard' element={<Dashboard />} />
         </Route>
