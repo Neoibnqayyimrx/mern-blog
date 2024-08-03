4abc686b271dce74a1f50c175491d6d6c96e9e01
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-08-03 20:52:38 +0100

    complete post creation

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -20,12 +21,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user',  userRoutes); 
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
