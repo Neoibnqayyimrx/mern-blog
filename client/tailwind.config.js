900cef539c1b60c92da154b9800f7190e5205419
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-08-09 10:54:20 +0100

    line clamp installation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'), require('tailwind-scrollbar')
+    require('flowbite/plugin'), 
+    require('tailwind-scrollbar')
   ],
 };
